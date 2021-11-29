5f90681c7f53683223ee995efd2a04f7e5e2d57d
İhlas Mert <ihlas1mert@gmail.com>
2021-11-29 19:58:37 +0300

    Initial Commit

---
+++ pickerUI/pickerUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pickerUIApp.swift
+//  pickerUI
+//
+//  Created by ihlas on 29.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct pickerUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
